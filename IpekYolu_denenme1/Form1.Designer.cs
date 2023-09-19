6aadc614f135ebfccca65a494275cd926846d917
admin <admin@LAPTOP-GSB71HNG>
2023-09-19 18:47:46 +0300

    Proje dosyası ekle.

---
+++ IpekYolu_denenme1/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace IpekYolu_denenme1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.slider = new System.Windows.Forms.PictureBox();
+            this.txtNemDegeri = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRuzgarDegeri = new System.Windows.Forms.TextBox();
+            this.slider2 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slider
+            // 
+            this.slider.Location = new System.Drawing.Point(47, 99);
+            this.slider.Name = "slider";
+            this.slider.Size = new System.Drawing.Size(671, 30);
+            this.slider.TabIndex = 0;
+            this.slider.TabStop = false;
+            this.slider.Paint += new System.Windows.Forms.PaintEventHandler(this.slide_Paint);
+            this.slider.MouseDown += new System.Windows.Forms.MouseEventHandler(this.slide_MouseDown);
+            this.slider.MouseMove += new System.Windows.Forms.MouseEventHandler(this.slide_MouseMove);
+            this.slider.MouseUp += new System.Windows.Forms.MouseEventHandler(this.slide_MouseUp);
+            // 
+            // txtNemDegeri
+            // 
+            this.txtNemDegeri.Enabled = false;
+            this.txtNemDegeri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNemDegeri.Location = new System.Drawing.Point(219, 57);
+            this.txtNemDegeri.Name = "txtNemDegeri";
+            this.txtNemDegeri.Size = new System.Drawing.Size(138, 28);
+            this.txtNemDegeri.TabIndex = 1;
+            this.txtNemDegeri.Text = "Nem değeri :";
+            this.txtNemDegeri.TextChanged += new System.EventHandler(this.txtGoster_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(44, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nem değeri :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(42, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Rüzgar şideti :";
+            // 
+            // txtRuzgarDegeri
+            // 
+            this.txtRuzgarDegeri.Enabled = false;
+            this.txtRuzgarDegeri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtRuzgarDegeri.Location = new System.Drawing.Point(217, 177);
+            this.txtRuzgarDegeri.Name = "txtRuzgarDegeri";
+            this.txtRuzgarDegeri.Size = new System.Drawing.Size(138, 28);
+            this.txtRuzgarDegeri.TabIndex = 4;
+            this.txtRuzgarDegeri.Text = "Rüzgar değeri :";
+            // 
+            // slider2
+            // 
+            this.slider2.Location = new System.Drawing.Point(45, 219);
+            this.slider2.Name = "slider2";
+            this.slider2.Size = new System.Drawing.Size(671, 30);
+            this.slider2.TabIndex = 3;
+            this.slider2.TabStop = false;
+            this.slider2.Paint += new System.Windows.Forms.PaintEventHandler(this.slider2_Paint);
+            this.slider2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.slider2_mouseDown);
+            this.slider2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.slider2_mouseMove);
+            this.slider2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.slider2_mouseUp);
+            // 
+            // chart1
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(777, 57);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series6.Legend = "Legend1";
+            series6.Name = "Nem";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(394, 269);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(42, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Sıcaklık Degeri :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(252, 309);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 28);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Sıcaklık :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(217, 384);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 30);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(42, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Port Sec        :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(42, 453);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(228, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Serial haberleşme  :";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(276, 455);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(138, 30);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::IpekYolu_denenme1.Properties.Resources.indir;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1230, 625);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1230, 625);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRuzgarDegeri);
+            this.Controls.Add(this.slider2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNemDegeri);
+            this.Controls.Add(this.slider);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox slider;
+        private System.Windows.Forms.TextBox txtNemDegeri;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRuzgarDegeri;
+        private System.Windows.Forms.PictureBox slider2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ IpekYolu_denenme1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.IO.Ports;
+using System.CodeDom;
+
+namespace IpekYolu_denenme1
+{
+
+    public partial class Form1 : Form
+    {
+        String[] portlar = SerialPort.GetPortNames();
+        float Default_value = 0.1f, Min = 0.0f, Max = 1.0f;
+        float Default_value2 = 0.1f, Min2 = 0.0f, Max2= 1.0f;
+        public float sıcaklık;
+        bool mouse = false;
+        bool mouse2 = false;
+        public float Bar(float value)
+        {
+            return (slider.Width - 24) * (value - Min) / (float)(Max - Min);
+        }
+        public float Bar2(float value)
+        {
+            return (slider2.Width - 24) * (value - Min2) / (float)(Max2-Min2);
+        }
+        public void thumb(float value)
+        {
+            if (value < Min) value = Min;
+            if (value > Max) value = Max;
+            Default_value = value;
+            slider.Refresh();
+        }
+        public void thumb2(float value)
+        {
+            if (value < Min2) value = Min2;
+            if (value > Max2) value = Max2;
+            Default_value2 = value;
+            slider2.Refresh();
+        }
+        public float slider_With(int x)
+        {
+            return Min + (Max - Min) * x / (float)(slider.Width);
+        }
+        public float slider_With2(int x)
+        {
+            return Min2 + (Max2 - Min2) * x / (float)(slider2.Width);
+        }
+        private Timer refreshTimer;
+        public Form1()
+        {
+            InitializeComponent();
+            slider.Height = 30;
+            txtNemDegeri.Text = (Default_value * 100).ToString();
+            txtRuzgarDegeri.Text = (Default_value2 * 100).ToString();
+            ///////////////////////////////////////////////////////////////
+            refreshTimer = new Timer();
+            refreshTimer.Interval = 1000;
+            refreshTimer.Tick += new EventHandler(timer1_Tick);
+            refreshTimer.Start();
+            if (serialPort1.IsOpen== false)
+            {
+                return;
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBox1.Text);
+
+            }
+            try
+            {
+                serialPort1.Open();
+
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message);
+            }
+            try
+            {
+                String sonuc = serialPort1.ReadExisting();
+;
+               float.TryParse(sonuc, out sıcaklık);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                timer1.Stop();
+                
+            }
+        }
+
+        private void slide_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = true;
+            thumb(slider_With(e.X));
+        }
+        private void slider2_mouseDown(object sender, MouseEventArgs e)
+        {
+            mouse2 = true;
+            thumb2(slider_With2(e.X));
+        }
+        private void slide_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouse)
+            {
+                return;
+            }
+            thumb(slider_With(e.X));
+        }
+        private void slider2_mouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouse2)
+            {
+                return;
+            }
+            thumb2(slider_With2(e.X));
+        }
+        private void slide_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse = false;
+            txtNemDegeri.Text = (Default_value*100).ToString();
+        }
+
+        private void slider2_mouseUp(object sender, MouseEventArgs e)
+        {
+            mouse2 = false;
+            txtRuzgarDegeri.Text = (Default_value2 * 100).ToString();
+        }
+
+
+        private void slide_Paint(object sender, PaintEventArgs e)
+        {
+            float bar_Size = 0.45f;
+            float x = Bar(Default_value);
+            int y = (int)(slider.Height * bar_Size);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.FillRectangle(Brushes.DimGray, 0, y, slider.Width, y / 2);
+            e.Graphics.FillRectangle(Brushes.Red, 0, y, x, slider.Height - 2 * y);
+            using (Pen pen = new Pen(Color.White, 5))
+            {
+                e.Graphics.DrawEllipse(pen, x + 4, y - 6, slider.Height / 2, slider.Height / 2);
+                e.Graphics.FillEllipse(Brushes.Red, x + 4, y - 6, slider.Height / 2, slider.Height / 2);
+            }
+            using (Pen pen = new Pen(Color.White, 5))
+            {
+                e.Graphics.DrawEllipse(pen, x + 4, y - 6, slider.Height / 2, slider.Height/2);
+            }
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void slider2_Paint(object sender, PaintEventArgs e)
+        {
+            float bar_Size = 0.45f;
+            float x = Bar(Default_value2);
+            int y = (int)(slider2.Height * bar_Size);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.FillRectangle(Brushes.DimGray, 0, y, slider.Width, y / 2);
+            e.Graphics.FillRectangle(Brushes.Red, 0, y, x, slider.Height - 2 * y);
+            using (Pen pen = new Pen(Color.White, 5))
+            {
+                e.Graphics.DrawEllipse(pen, x + 4, y - 6, slider.Height / 2, slider.Height / 2);
+                e.Graphics.FillEllipse(Brushes.Red, x + 4, y - 6, slider.Height / 2, slider.Height / 2);
+            }
+            using (Pen pen = new Pen(Color.White, 5))
+            {
+                e.Graphics.DrawEllipse(pen, x + 4, y - 6, slider.Height / 2, slider.Height / 2);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen==true)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] categories = { "sıcaklık", "rüzgar", "sıcaklık" };
+            float[] data = { Default_value*100, Default_value2*100, sıcaklık };
+            Chart chart = chart1;
+            chart.Series[0].Points.Clear();
+            for (int i = 0; i < categories.Length; i++)
+            {
+                chart.Series[0].Points.AddXY(categories[i], data[i]);
+            }
+        }
+        
+        private void txtGoster_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            foreach (String port in portlar)
+            {
+                comboBox1.Items.Add(port);
+                comboBox1.SelectedIndex = 0;
+            }
+            comboBox2.Items.Add("115200");
+            comboBox2.Items.Add("4800");
+            comboBox2.Items.Add("9600");
+            comboBox2.SelectedIndex = 0;
+
+
+        }
+        
+
+        
+
+
+    }
+}
